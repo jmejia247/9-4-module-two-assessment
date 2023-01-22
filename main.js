e122bc6cbe59550e1de0f2d04d23ff3614b2ad67
Angel <EMAIL>
2023-01-22 12:26:16 -0500

    titles api

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
-
-
-
-
-
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
+const titles = document.querySelector('#titles');
+const input = document.querySelector('#userInput');
+const movieTitle = document.querySelector('#movie-title');
+const releaseDate = document.querySelector('#release-date');
+const description = document.querySelector('#description');
+const contact = document.querySelector('#contact');
+const form = document.querySelector('#form');
+const reviews = document.querySelector('#reviews');
+const addReview = document.querySelector('#review');
+const ownReview = document.querySelector('#ownReview');
+const peopleShow = document.querySelector('#people-show');
+const userData = document.querySelector('#userData');
 
-function run() {
- // Add code you want to run on page load here
+
+function getAPI() {
+    fetch('https://resource-ghibli-api.onrender.com/films')
+    .then((response) => response.json())
+    .then((response) => {
+        console.log(response)
+        response.forEach((movie)=> {
+            let option = document.createElement('option');
+            option.setAttribute('value', movie.id);
+            option.innerHTML = movie.title;
+            titles.append(option);
+        });
+    });
 }
+
+titles.addEventListener('change', (event) => {
+    fetch(`https://resource-ghibli-api.onrender.com/films/${titles.value}`)
+    .then((response) => response.json())
+    .then((response) => {
+        if(titles.value)
+        movieTitle.innerHTML = response.title;
+        releaseDate.innerHTML = response.release_Date;
+        description.innerHTML = response.description;
+    });
+});
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
 // A non-hacky solution is being researched
 
-setTimeout(run, 1000);
+setTimeout(getAPI, 1000);
