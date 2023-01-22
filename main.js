afab940703fff369a0bc68d4863b8c0e1c682bbd
JohnnyMCR <jmcastillo991@gmail.com>
2023-01-22 14:21:35 -0500

    Some checks

--- main.js
+++ main.js
@@ -6,29 +6,38 @@
 
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
+const BASE_URL = "https://resource-ghibli-api.onrender.com/films"
+const PEOPLE_URL = "https://resource-ghibli-api.onrender.com/people/"
+const select = document.querySelector('select');
+const form = document.querySelector('form');
+
 function run() {
- // Add code you want to run on page load here
- let dropdown = document.querySelector(".dropdown")
+    // Add code you want to run on page load here
+    let dropdown = document.querySelector(".dropdown")
 
-fetch('https://resource-ghibli-api.onrender.com/people').then(response => response.json()).then(data => {
-    // console.log(data.results)
-    data.results.forEach(character => {
-        // console.log(character)
-        let tag = document.createElement("option")
-        tag.innerText = character.name
-        tag.value = character.id
-        dropdown.append(tag)
-    });
-})
-
-dropdown.addEventListener("change", (e) => {
-    console.log(dropdown.value)
-
-    fetch('https://resource-ghibli-api.onrender.com/people/${dropdown.value}').then(response => response.json()).then(people => {
-        console.log(people)
-        
+    fetch(BASE_URL).then(response => response.json()).then(movieInfo => {
+        for(let i = 0; i < movieInfo.length; i++){
+            //const title = data[i].title;
+            const option = document.createElement('option');
+            select.append(option);
+            option.textContent = movieInfo[i].title;
+            option.setAttribute('value', movieInfo[i].id);
+    
+            movieDescription(movieInfo);
+        }
     })
-})
+    .catch((error)=> console.log(error))
+}
+    function movieDescription(movieInfo) {
+        select.addEventListener('change', (event) => {
+            event.preventDefault();
+    
+            const info = document.querySelector('#display-info');
+            info.innerHTML = ''
+            const h3 = document.createElement('h3')
+            const pTag = document.createElement('p')
+            const pTag2 = document.createElement('p')
+        })
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
