391ec1fe5eb2b25ae02209d9f7f1072740f6f8eb
shareekaepps <shareekaepps@icloud.com>
2023-01-22 16:13:03 -0500

    found error preventin g pass, added css style to fix last test, assessment complete all test pass

--- main.js
+++ main.js
@@ -1,39 +1,11 @@
-// [
-//     {
-//       "id": "2baf70d1-42bb-4437-b551-e5fed5a87abe",
-//       "title": "Castle in the Sky",
-//       "original_title": "天空の城ラピュタ",
-//       "original_title_romanised": "Tenkū no shiro Rapyuta",
-//       "description": "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.",
-//       "director": "Hayao Miyazaki",
-//       "producer": "Isao Takahata",
-//       "release_date": "1986",
-//       "running_time": "124",
-//       "rt_score": "95",
-//       "people": [
-//         "/people/"
-//       ],
-//       "species": [
-//         "/species/af3910a6-429f-4c74-9ad5-dfe1c4aa04f2"
-//       ],
-//       "locations": [
-//         "/locations/"
-//       ],
-//       "vehicles": [
-//         "/vehicles/"
-//       ],
-//       "url": "/films/2baf70d1-42bb-4437-b551-e5fed5a87abe"
-//     },
-//   ]
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 const displayInfo = document.querySelector('#display-info');
-const getTitles = document.querySelector('select');
+const getMovies = document.querySelector('select');
 const reviewForm = document.querySelector('form');
 const getPeople = document.querySelector('#show-people');
 const reviews = document.querySelector('#review');
-const reset = document.querySelector('#reset-reviews');
+const resetButton = document.querySelector('#reset-reviews');
 const ul = document.querySelector('ul');
 const ol = document.querySelector('ol');
 
@@ -45,14 +17,14 @@ function run() {
  fetch("https://resource-ghibli-api.onrender.com/films")
     .then(response => response.json())
     .then(movies => {
-        // console.log(movies);
+        console.log(movies);
     movies.forEach(movie => {
         let currMovie = document.createElement('option');
         currMovie.value = movie.title;
         currMovie.innerHTML = movie.title;
-        getTitles.append(currMovie);
+        getMovies.append(currMovie);
 
-    getTitles.addEventListener('change', (e) => {
+    getMovies.addEventListener('change', (e) => {
         if(e.target.value === currMovie.value) {
             displayInfo.innerHTML = '';
             
@@ -72,15 +44,49 @@ function run() {
     reviewForm.addEventListener('submit', (e) => {
         e.preventDefault()
 
-        const userInput = e.target.review.value;
+        const input = e.target.review.value;
         const li = document.createElement('li');
 
-
+        let movies = getMovies.value;
+        if(movies === '') {
+            alert('Please select a movie first')
+        } else {
+            li.innerHTML = `<strong><b>${movies}</b></strong>: ${input}`
+            ul.append(li);
+            e.target.reset();
+            }
+        });
     })
+    .catch(error => {
+    console.log(error)
+    });
 
-    })
-
+    
+    resetButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        ul.innerHTML = '';
+    });
 }
+
+function peopleNames() {
+    fetch("https://resource-ghibli-api.onrender.com/people")
+        .then(response => response.json())
+        .then(data => {
+
+            for(let i = 0; i < data.length; i++) {
+                const peopleList = document.createElement('li');
+                peopleList.innerHTML = data[i].name;
+                ol.append(peopleList);
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        });
+}
+        getPeople.addEventListener('click', (e) => {
+        e.preventDefault();
+        peopleNames();
+})
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
