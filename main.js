d005d3bfc5246adb656d31c059c0f2aec09c6c97
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-01-22 11:39:29 -0500

    9-4-module-two-assessment

--- main.js
+++ main.js
@@ -7,6 +7,16 @@
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
+
+    let titleSelect = document.querySelector("#titles");
+    let movieInfo = document.querySelector("#display-info");
+    let submitReview = document.querySelector("section form");
+    let addReview = document.querySelector("#reviews ul");
+    let resetReview = document.querySelector("#reviews button");
+    let addPeople = document.querySelector("#show-people");
+    let orderList = document.querySelector("#ol");
+    addPeople.before(orderList);
+
     fetch("https://resource-ghibli-api.onrender.com/films")
     .then(response => response.json())
     .then (response => {
@@ -17,7 +27,76 @@ function run() {
                 option.innerHTML = movie.title;
                 titleSelect.append(option)
         })
+
+        titleSelect.addEventListener("change", (event) => {
+            event.preventDefault();
+   
+            response.forEach(movie => {
+                if(titleSelect.value === movie.title){
+                    movieInfo.innerHTML = "";
+   
+                    const title = document.createElement("h3")
+                    const releaseDate = document.createElement("p");
+                    const description = document.createElement("p");
+   
+                    title.innerText = movie.title;
+                    movieInfo.append(title);
+   
+                    releaseDate.innerText = movie.release_date;
+                    movieInfo.append(releaseDate);
+   
+                    description.innerText = movie.description;
+                    movieInfo.append(description);
+                }
+            })
+        })
+   
+        submitReview.addEventListener("submit", (event) => {
+            event.preventDefault();
+            if(titleSelect.value === ""){
+                alert("Please select a movie first")
+            }
+            else{
+            const typedReview = document.querySelector("form input[type='text']");
+            review = document.createElement("li");
+            review.innerHTML = `<strong>${titleSelect.value}:</strong> ${typedReview.value}`
+            addReview.append(review);
+            typedReview.value = "";
+            }
+        })
+   
+        resetReview.addEventListener("click", (event) => {
+            event.preventDefault();
+            addReview.innerHTML = "";
+   
+        })
+   
+        addPeople.addEventListener("click", (event) => {
+            ol.innerHTML = "";
+   
+            fetch("https://ghibliapi.herokuapp.com/people/")
+            .then(resp => resp.json())
+            .then(resp => {
+                for( let movie of response){
+   
+                    if(titleSelect.value === movie.title){
+                            console.log(movie.id);
+                            for(let per of resp){
+                                urlId = per.films[0].split("/")
+                                if(movie.id === urlId[urlId.length-1]){
+                                    const li = document.createElement("li");
+                                    ol.append(li);
+                                    li.innerText = per.name;
+                                }
+                            }
+   
+                        }
+                    }
+   
+            })
+        })
     })
+    .catch(console.log)
 
  // Add code you want to run on page load here
 }
