74b31bcb8417a1c543b588e189fb3f450105e9ea
JenniferEinstein <jennifereinstein@gmail.com>
2023-01-22 10:05:19 -0500

    Getting the basics in.

--- main.js
+++ main.js
@@ -4,10 +4,11 @@
 
 
 
-// To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
+// To ensure Cypress tests work as expected, add any code/functions that you would like to run on page load inside this function
 
 function run() {
  // Add code you want to run on page load here
+ 
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
