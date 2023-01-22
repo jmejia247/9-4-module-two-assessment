94f713716d46411ed97428b22ea04375250f62e1
JenniferEinstein <jennifereinstein@gmail.com>
2023-01-22 17:53:45 -0500

    Not great, no.

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ consider: addEventListener for change
 
 function run() {
  // Add code you want to run on page load here
-
 //fetch, then take the fetch and populate the dropdown
 
 fetch(`https://resource-ghibli-api.onrender.com/films`)
@@ -15,44 +14,50 @@ fetch(`https://resource-ghibli-api.onrender.com/films`)
     .then(movies => {
         console.log("fetch success! data is", movies)
         let dropdown = document.getElementById("titles")
-        for(i=0; i<movies.length; i++){
-            const movieOptions = document.createElement("options");
-            movieOptions.innerText = movie.title
-            movieOptions.value = movie.id
-            dropdown.append(movieOptions)
-            
-        }
-    })
-
-
-
-//console.log(response)
-//console.log(data)
-//console.log(data.response)
-//console.log(data.response.title)
-//console.log(data.results.title)
-// data.results.forEach((id) => {
-  //  console.log(id)
-
-const titleH3 = document.querySelector("h3")
-const yearP = document.querySelector("p.displayP1")
-const describeP = document.querySelector("p.displayP2")
-
-
-//console.log(movieOptions.innerText=films.title)
-//console.log(movieOptions.value=films.id)
-//    console.log(dropdown.append(movieOptions))
+        // movies.response.forEach(title => {
+        // for(i=0; i<movies.length; i++){
+            movies.forEach((movie) => {
+            let movieOption = document.createElement("option");
+            movieOption.innerText = movie.title
+            movieOption.value = movie.id
+            dropdown.append(movieOption)
+});
 
 //then to get people,I want to fetch 
-// fetch (`https://resource-ghibli-api.onrender.com/films/title`)
-//     .then(result => result.json())
-//     .then(data2=> console.log("second fetch successful 2nd data is ", data2))
+    dropdown.addEventListener("change", (event) => {
+        console.log("dropdown.id ",dropdown.id)
+        console.log("dropwdown.name", dropdown.name)
+        console.log("dropdown.value is" , dropdown.value)
+        fetch (`https://resource-ghibli-api.onrender.com/films/${dropdown.value}`)
+        .then(result => result.json())
+        .then(people => {
+            console.log("second fetch successful 2nd data is ", people)
+            let dropdown2 = document.getElementsByTagName("ol");
+            dropdown2.innerText = people.people;
+            dropdown2.id = people.id
+            dropdown.append(dropdown2)})
 
-    let peopleOptions = document.createElement("li")
-  //  document.getElementbytag("ol").append(peopleOptions)
-    //peopleOptions.innerText=films.title.pepple
-    //peopleOptions.value=films.title.pepple
-    //dropdown.append(peopleOptions)
+    // let peopleOptions = document.createElement("li")
+    //     document.getElementbytag("ol").append(peopleOptions)
+    //     peopleOptions.innerText=films.title.pepple
+      //peopleOptions.value=films.title.pepple
+      //dropdown.append(peopleOptions)
+  
+
+
+
+      const titleH3 = document.querySelector("h3")
+      const yearP = document.querySelector("p.displayP1")
+      const describeP = document.querySelector("p.displayP2")
+  
+      titleH3 = people.title
+      yearP = people.release-date
+      describeP = people.description
+
+
+
+
+
 
 // document.getElementById("review").addEventListener("submit",function(){ document.getElementById
 
@@ -71,7 +76,7 @@ document.getElementById("reviewSubmit").addEventListener("submit",function(){
     let movieReviewed= document.getElementById("displayH3")
     nextReview.innerHTML=`${movieReviewed}: ${document.getElementsByID("review")}`
     reviewSection.append(nextReview)
-})}
+})
 
 
 document.getElementById("reset-reviews").addEventListener("click",function(){})
@@ -79,5 +84,7 @@ document.getElementById("reset-reviews").addEventListener("click",function(){})
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
 // A non-hacky solution is being researched
+    })
+})}
 
 setTimeout(run, 1000);
