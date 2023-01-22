43bf06265740cc42b98236bb45f2988dc1f89bea
JenniferEinstein <jennifereinstein@gmail.com>
2023-01-22 16:39:18 -0500

    Move code around. Fetch works better. Work on for loop vs forEach.

--- main.js
+++ main.js
@@ -8,25 +8,23 @@ consider: addEventListener for change
 function run() {
  // Add code you want to run on page load here
 
-    let dropdown = document.getElementById("titles")
-    //dropdown = ""
-
-        // document.querySelector("form").addEventListener("onload", (e) => {
-        //     e.preventDefault();
-        //     console.log(`https://resource-ghibli-api.onrender.com/films`, "Made it!"
-        //     );
-
-/* standard fetch set up is: 
-fetch()
-.then((response) => response.json())
-.then((data => console.log(data))    */ 
-
 //fetch, then take the fetch and populate the dropdown
 
 fetch(`https://resource-ghibli-api.onrender.com/films`)
     .then((response) => response.json())
-    .then((data => console.log("fetch success! data is", data)))
-}
+    .then(movies => {
+        console.log("fetch success! data is", movies)
+        let dropdown = document.getElementById("titles")
+        for(i=0; i<movies.length; i++){
+            const movieOptions = document.createElement("options");
+            movieOptions.innerText = movie.title
+            movieOptions.value = movie.id
+            dropdown.append(movieOptions)
+            
+        }
+    })
+
+
 
 //console.log(response)
 //console.log(data)
@@ -40,29 +38,43 @@ const titleH3 = document.querySelector("h3")
 const yearP = document.querySelector("p.displayP1")
 const describeP = document.querySelector("p.displayP2")
 
-let movieOptions = document.createElement("options");
-//movieOptions.innerText=films.title
-//movieOptions.value=films.id
-//dropdown.append(movieOptions)
-
 
 //console.log(movieOptions.innerText=films.title)
 //console.log(movieOptions.value=films.id)
 //    console.log(dropdown.append(movieOptions))
 
 //then to get people,I want to fetch 
-fetch (`https://resource-ghibli-api.onrender.com/films/title`)
-    .then(result => result.json())
-    .then(data2=> console.log("second fetch successful 2nd data is ", data2))
+// fetch (`https://resource-ghibli-api.onrender.com/films/title`)
+//     .then(result => result.json())
+//     .then(data2=> console.log("second fetch successful 2nd data is ", data2))
 
-    let peopleOptions = document.createElement("options");
+    let peopleOptions = document.createElement("li")
+  //  document.getElementbytag("ol").append(peopleOptions)
     //peopleOptions.innerText=films.title.pepple
     //peopleOptions.value=films.title.pepple
     //dropdown.append(peopleOptions)
 
-document.getElementById("review").addEventListener("click",function(){
-    document.getElementById
-})
+// document.getElementById("review").addEventListener("submit",function(){ document.getElementById
+
+
+
+document.getElementById("reviewSubmit").addEventListener("submit",function(){
+  
+    try {
+        if(getElementBytag("h3")==""){
+            return "Please pick a movie"
+        };
+    } catch (error) {
+        console.error(error);
+    }
+    let nextReview = document.createElement("p");
+    let movieReviewed= document.getElementById("displayH3")
+    nextReview.innerHTML=`${movieReviewed}: ${document.getElementsByID("review")}`
+    reviewSection.append(nextReview)
+})}
+
+
+document.getElementById("reset-reviews").addEventListener("click",function(){})
  
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
