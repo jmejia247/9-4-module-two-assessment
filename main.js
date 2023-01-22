b14ea27aa5c26ba4a75046ed767308f791dc1ac3
USERNAME <EMAIL>
2023-01-22 14:48:49 -0500

    part done

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
-
-
-
-
-
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
-
 function run() {
+
+
 
     const BASE_URL = 'https://resource-ghibli-api.onrender.com/films';
 
+    let dropdown = document.querySelector(".dropdown")
+
+    fetch(`${BASE_URL}`)
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+        
+        // data.results.forEach(character => {
+        //     // console.log(character)
+        //     let tag = document.createElement("option")
+        //     tag.innerText = character.name
+        //     tag.value = character.id
+        //     dropdown.append(tag)
+        // });
+    })
+
+
+
     
-
-
-
-
-
-
-
-
-
-
-
-
-
- // Add code you want to run on page load here
-}
-
-// This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
-// So that testing can work as expected for now
-// A non-hacky solution is being researched
-
-setTimeout(run, 1000);
+            }
+    // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
+    // So that testing can work as expected for now
+    // A non-hacky solution is being researched
+    
+    setTimeout(run, 1000);
