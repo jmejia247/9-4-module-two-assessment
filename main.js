df886ae9235d815ad95c41fe03f160254063ca66
JohnnyMCR <jmcastillo991@gmail.com>
2023-01-22 12:46:07 -0500

    first commit

--- main.js
+++ main.js
@@ -8,6 +8,27 @@
 
 function run() {
  // Add code you want to run on page load here
+ let dropdown = document.querySelector(".dropdown")
+
+fetch('https://resource-ghibli-api.onrender.com/people').then(response => response.json()).then(data => {
+    // console.log(data.results)
+    data.results.forEach(character => {
+        // console.log(character)
+        let tag = document.createElement("option")
+        tag.innerText = character.name
+        tag.value = character.id
+        dropdown.append(tag)
+    });
+})
+
+dropdown.addEventListener("change", (e) => {
+    console.log(dropdown.value)
+
+    fetch('https://resource-ghibli-api.onrender.com/people/${dropdown.value}').then(response => response.json()).then(people => {
+        console.log(people)
+        
+    })
+})
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
