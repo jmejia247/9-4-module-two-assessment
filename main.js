2f57671d0da4deebe6632193385e9e3f53a7b84b
keriarmstrong <khetto@live.com>
2023-01-23 10:15:11 -0500

    added and deleted some comments and simplify some lines

--- main.js
+++ main.js
@@ -9,10 +9,13 @@
 function run() {
  // Add code you want to run on page load here
 
+ //------------------------------DEFINE STARTER VARIABLES-------------------
  console.log('js is connected')
  const URL = 'https://resource-ghibli-api.onrender.com'
+ let ol = document.querySelector('.list ol')
  let dropDown = document.querySelector('#titles')
 
+ //--------------------------------CREATE INTITAL FETCH REQUEST---------------
  fetch(`${URL}/films`)
 .then(resp => resp.json())
 .then(data => {
@@ -25,8 +28,10 @@ function run() {
     });
 })
 
+//------------------------------DROPDOWN EVENT---------------------
 dropDown.addEventListener('change', (event) => {
     event.preventDefault()
+    ol.innerHTML = ""
     let selectedMovie = dropDown.value
 
     fetch(`${URL}/films`)
@@ -34,7 +39,8 @@ dropDown.addEventListener('change', (event) => {
     .then(data => {
         let titles = (data)
         // console.log(titles[0].title)
-//-------------------------------GET MOVIE DETAILS---------------------   
+
+//-------------------------------GET MOVIE DATA---------------------   
 function getData() {
     for(let i = 0; i<titles.length; i++){
 if(titles[i].title == selectedMovie)
@@ -44,6 +50,7 @@ return (titles[i])
 
 let movieDetails = document.querySelector('#display-info')
 movieDetails.innerHTML = ""
+// button.innerHTML = ""
 
     let title = (getData().title)
     let titleHeader = document.createElement('h3')
@@ -60,39 +67,30 @@ let release = (getData().release_date)
     releaseDate.innerHTML = `${release}`
 
     movieDetails.append(titleHeader, releaseDate, movieDescription)
-//----------------------------PEOPLE-------------------
-// fetch(`${URL}people`)
-// .then(resp => resp.json())
-// .then(data => {
-// console.log(data)
-// })
+//-----------------------GET PEOPLE LIST EVENT-------------------
+const button = document.querySelector('#show-people')
+
+button.addEventListener('click', (event) =>{
+    event.preventDefault()
+    ol.innerHTML = ""
+return populateList()
+})
 
 let peopleData = (getData().people)
-        
-        // console.log(peopleData[0])
-        // console.log(`${URL}${peopleData}`)
- function listPerson()  {
-    for(i=0; i< peopleData.length; i++){
-    let   newURL = `${URL+peopleData[i]}`
-        fetch(newURL)
-        .then(resp => resp.json())
-        .then(data => {
-            console.log(data.name)
-        })
-    }
- }   
- console.log(listPerson()) 
 
-//         peopleData.forEach(
-// fetch(`${URL}${peopleData}`)
-// .then(resp => resp.json())
-// .then(data => {
-// console.log(data)
-// })
-// )
 
-    //  ) }
-// }
+function populateList() {peopleData.forEach(url => {
+    fetch(URL+url)
+    .then(resp => resp.json())
+    .then(data => {
+        let person = (data.name)
+        let li = document.createElement('li')
+        li.innerHTML = `${person}`
+        ol.append(li)
+    })
+}
+   
+)}
 
 const form = document.querySelector('#review')
 
